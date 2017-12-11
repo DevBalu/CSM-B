8e6c91c8952f1b1c82056daf9dcc05f3566a23f8
DevBalu <ghenov.andrei@gmail.com>
2017-12-11 13:06:58 +0200

    /

--- web/index.php
+++ web/index.php
@@ -1,6 +1,17 @@
 <?php 
+require('../vendor/autoload.php');
 
-$app->get('/cowsay', function() use($app) {
-  $app['monolog']->addDebug('cowsay');
-  return "<pre>".\Cowsayphp\Cow::say("Cool beans")."</pre>";
+$app = new Silex\Application();
+$app['debug'] = true;
+
+// Register the monolog logging service
+$app->register(new Silex\Provider\MonologServiceProvider(), array(
+  'monolog.logfile' => 'php://stderr',
+));
+
+$app->get('/', function() use($app) {
+	$app['monolog']->addDebug('cowsay');
+	print "vasea";
+
+	return "vasea- return";
 });
